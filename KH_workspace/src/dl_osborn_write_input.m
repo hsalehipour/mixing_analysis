69a17d756539e6dc3764e21f5aa6d0e51e69c651
Hesam <hesam.salehipour@autodesk.com>
2018-07-28 15:31:24 -0400

    normalized ML training data by integral sum

--- KH_workspace/src/dl_osborn_write_input.m
+++ KH_workspace/src/dl_osborn_write_input.m
@@ -18,7 +18,8 @@ function dl_osborn_write_input(fname, my_eff, epsbar,N2, kappa0,z,Iu)
     DD = max(0., DD);      % eps, turbul dissip.
     
     % fix the vertical resolution for all cases (Lz=30 often)
-    dz = 30/(nz_profile-1);
+    Lz = max(z)-min(z);
+    dz = Lz/(nz_profile-1);
     
     nrand_extra = 10;
     for i=1:ntime;
@@ -30,14 +31,14 @@ function dl_osborn_write_input(fname, my_eff, epsbar,N2, kappa0,z,Iu)
             
             % number of data points in the selected range
             nzr = round((max(z(indz))-min(z(indz)))/dz)+1;
-            N2avg = mean(N2(indz,i));
-            Dp    = kappa0*N2avg;
+            N2tot = trapz(z(indz),N2(indz,i),1);
+            Dp    = kappa0*N2tot;
             features = zeros(nz_profile, 3);
             
             % interpolate to the selected z-range
             zft = linspace(min(z(indz)),max(z(indz)),nzr);
             ft1 = interp1(z(indz), DD(indz,i)/Dp, zft);
-            ft2 = interp1(z(indz), N2(indz,i)/N2avg, zft);
+            ft2 = interp1(z(indz), N2(indz,i)/N2tot, zft);
             features(1:nzr,1:2) = [ft1', ft2'];
             features(:,3) = my_eff(i);
             fprintf(fid1, '%17.9e, \t %17.9e, \t %17.9e \r\n', features');
