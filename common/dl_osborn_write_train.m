eda2540b97ae6661e6d4cf5c3b0f678892d9aa9d
Hesam <hesam.salehipour@autodesk.com>
2018-08-10 18:34:32 -0400

    Important: corrected mixing for dl_osborn to account avg_fac

--- common/dl_osborn_write_train.m
+++ common/dl_osborn_write_train.m
@@ -27,13 +27,22 @@ function dl_osborn_write_train(fname, my_eff, M, epsbar,N2, kappa0,z,zmax_shl,zm
         
         % interpolate to the selected z-range
         zft = linspace(min(z(indz)),max(z(indz)),nz_profile);
-        ft1 = zft./L_shl;
-        ft2 = interp1(z(indz), DD(indz,i)/Dp, zft);
-        ft3 = interp1(z(indz), N2(indz,i)/N2tot, zft);
-        features(:, 1)  = ft1;
-        features(:,2:3) = [ft2', ft3'];
-        features(:,4) = my_eff(i);
-        features(:,5) = MM(i)/Dp;
+        dns_zz = zft./L_shl;
+        dns_eps = interp1(z(indz), DD(indz,i)/Dp, zft);
+        dns_N2  = interp1(z(indz), N2(indz,i)/N2tot, zft);
+        dns_eff = my_eff(i);
+        avg_fac = mean(dns_eps)/mean(DD(:,i)/Dp);
+        dns_MM = MM(i)/Dp*avg_fac;
+        
+        % To check that the avg factor is working properly
+        % figure(1); hold all;
+        % plot(i,my_eff(i),'ro',i,dns_MM/(dns_MM+mean(dns_eps)),'b*');
+        
+        % assemble all input data
+        features(:, 1)  = dns_zz;
+        features(:,2:3) = [dns_eps', dns_N2'];
+        features(:,4) = dns_eff;
+        features(:,5) = dns_MM;
         fprintf(fid1, '%17.9e, \t %17.9e, \t %17.9e, \t %17.9e, \t %17.9e \r\n', features');
     end
     fclose(fid1);
